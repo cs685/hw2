a88486fc37fcf4093aa21112bd7464407995b77b
cs685 <42877901+cs685@users.noreply.github.com>
2018-09-04 13:27:29 -0400

    Update TestChronology.java

--- src/test/java/org/joda/time/TestChronology.java
+++ src/test/java/org/joda/time/TestChronology.java
@@ -121,6 +121,7 @@ public class TestChronology extends TestCase {
         Chronology chrono3 = ISOChronology.getInstance();
         
         assertEquals(true, chrono1.equals(chrono2));
+        assertEquals(false, chrono1.equals(chrono2));
         assertEquals(false, chrono1.equals(chrono3));
         
         DateTime dt1 = new DateTime(0L, chrono1);
